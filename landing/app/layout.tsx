6d1d90252d249848127d6160e370027e0ab83612
Coding With Josh <codewithjoshh@gmail.com>
2024-06-16 20:23:28 +0000

    nav

--- landing/app/layout.tsx
+++ landing/app/layout.tsx
@@ -7,8 +7,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "The Twist Platform",
+  description: "Transact, Engage, Create and Learn- But with a Twist.",
 };
 
 export default function RootLayout({
---
+++ landing/data/index.ts
@@ -0,0 +1,27 @@
+export const navLinks = () => [
+    {
+        name: 'Home',
+        path: '/',
+        isButton: false
+    },
+    {
+        name: 'About',
+        path: '/about',
+        isButton: false
+    },
+    {
+        name: 'Features',
+        path: '/features',
+        isButton: false
+    },
+    {
+        name: 'Pricing',
+        path: '/pricing',
+        isButton: false
+    },
+    {
+        name: 'Get Started',
+        path: '/auth',
+        isButton: true
+    },
+]
